7503f5a9fe49389340c43a52bef277432285ec7b
Ivan Lutsenka <wani4ka@duck.com>
2023-04-12 00:18:16 +0300

    Add database integration tests

---
+++ scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/DatabaseTest.java
@@ -0,0 +1,30 @@
+package ru.tinkoff.edu.java.scrapper;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DatabaseTest extends IntegrationEnvironment {
+
+    private static final Set<String> expectedTableNames = Set.of("chat", "databasechangelog", "databasechangeloglock", "link");
+
+    @SneakyThrows
+    @Test
+    void testTableNames() {
+        Set<String> names = new HashSet<>();
+        try (
+                var con = getConnection();
+                var rs = con.getMetaData().getTables(null, "public", "%", new String[]{"TABLE"})
+        ) {
+            while (rs.next()) {
+                names.add(rs.getString("TABLE_NAME"));
+            }
+        }
+        assertAll(names.stream().map(name -> () -> assertTrue(expectedTableNames.contains(name))));
+        assertAll(expectedTableNames.stream().map(name -> () -> assertTrue(names.contains(name))));
+    }
+}
