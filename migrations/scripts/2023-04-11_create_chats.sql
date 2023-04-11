d2ac7d60684dba6a96e15a8c8fa18238e2082db0
Ivan Lutsenka <wani4ka@duck.com>
2023-04-11 23:22:43 +0300

    Add migrations

---
+++ migrations/scripts/2023-04-11_create_chats.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+
+--changeset wani4ka: create_chats
+
+create table chat (
+    id bigint not null primary key
+);
---
+++ migrations/scripts/2023-04-11_create_links.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+
+--changeset wani4ka: create_links
+create sequence link_id_seq;
+create table link (
+    id bigint not null primary key default nextval('link_id_seq'),
+    url text not null,
+    chat_id bigint,
+    constraint fk_chat
+                  foreign key (chat_id) references chat(id) on delete cascade
+);
