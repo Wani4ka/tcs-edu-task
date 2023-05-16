ce0d417816b0ca0a9179c1e29a659cc1c878be95
Ivan Lutsenka <wani4ka@duck.com>
2023-05-16 18:00:49 +0300

    Add received updates counter

--- bot/src/main/java/ru/tinkoff/edu/java/bot/chat/Telegram.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/chat/Telegram.java
@@ -3,8 +3,12 @@ package ru.tinkoff.edu.java.bot.chat;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.BotCommand;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.BaseRequest;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.request.SetMyCommands;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,17 +19,24 @@ import ru.tinkoff.edu.java.bot.processor.TelegramProcessor;
 public class Telegram {
     private final TelegramBot bot;
     private final TelegramProcessor processor;
+    private final MeterRegistry meterRegistry;
+    private final Counter updatesCounter = meterRegistry.counter("updates_count");
 
     @PostConstruct
     public void init() {
         bot.setUpdatesListener(updates -> {
-            updates.stream().map(processor::processUpdate).forEach(bot::execute);
+            updates.stream().map(this::processUpdate).forEach(bot::execute);
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
         });
         var commands = processor.getCommands().stream()
                         .map(cmd -> new BotCommand(cmd.getName(), cmd.getDescription()))
                         .toArray(BotCommand[]::new);
         bot.execute(new SetMyCommands(commands));
+    }
+
+    private BaseRequest<?, ?> processUpdate(Update update) {
+        updatesCounter.increment();
+        return processor.processUpdate(update);
     }
 
     public void sendMessage(long chatId, String text) {
